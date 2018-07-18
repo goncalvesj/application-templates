5ff3451b1dc8d11cf50c4a291dd80586b7e2468b
Joao Goncalves <joao.goncalves@oireachtas.ie>
2018-07-18 17:09:11 +0100

    Add project files.

---
+++ netcore-angular-elements/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace netcore_angular_elements.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
