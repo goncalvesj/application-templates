9e79de064bafac67c72a020c3825fe424192bebf
Joao Goncalves <jp.reisgoncalves@gmail.com>
2020-03-02 21:51:36 +0000

    added net core angular with aad integration

---
+++ NetCore-Angular-AzureAd/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcAngCliAad.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
