f5724ccf6b287a8c4583f8b152baf1bfcc991ca1
Joao Goncalves <jp.reisgoncalves@gmail.com>
2020-06-01 16:58:05 +0100

    Create netcore-angular-b2c.yml (#12)
    * Create netcore-angular-b2c.yml
    * b2c updated

--- NetCore-Angular-AzureB2C/ClientApp/src/app/home/home.component.ts
+++ NetCore-Angular-AzureB2C/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MsalService } from '@azure/msal-angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  constructor(private readonly msalService: MsalService) {
+    console.log(msalService.getUser());
+  }
 }
