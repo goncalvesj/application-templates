a0a56020eac01467e8cfc927b5ffe3e6482a4e03
Joao Goncalves <jgoncalves@microsoft.com>
2022-10-26 00:26:51 +0100

    worker rename.

--- AzureServiceBus-Worker-Notification/Net.ServiceBus.Worker/Worker.cs
+++ AzureServiceBus-Worker-Notification/Net.ServiceBus.Worker/Worker.cs
@@ -2,6 +2,7 @@ using Azure.Messaging.ServiceBus;
 using Azure.Storage.Blobs;
 using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.DataContracts;
+using Net.ServiceBus.Entities;
 using Newtonsoft.Json;
 
 namespace NetCore.ServiceBus.Worker
@@ -129,36 +130,5 @@ namespace NetCore.ServiceBus.Worker
                 _logger.LogError(ex, "Unable to handle message");
             }
         }
-    }
-
-    public class Data
-    {
-        public string api { get; set; }
-        public string clientRequestId { get; set; }
-        public string requestId { get; set; }
-        public string eTag { get; set; }
-        public string contentType { get; set; }
-        public int contentLength { get; set; }
-        public string blobType { get; set; }
-        public string url { get; set; }
-        public string sequencer { get; set; }
-        public StorageDiagnostics storageDiagnostics { get; set; }
-    }
-
-    public class Root
-    {
-        public string topic { get; set; }
-        public string subject { get; set; }
-        public string eventType { get; set; }
-        public string id { get; set; }
-        public Data data { get; set; }
-        public string dataVersion { get; set; }
-        public string metadataVersion { get; set; }
-        public DateTime eventTime { get; set; }
-    }
-
-    public class StorageDiagnostics
-    {
-        public string batchId { get; set; }
     }
 }
