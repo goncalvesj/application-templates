49529884a87975870e495709170d7d39e83c45a6
Joao Goncalves <jgoncalves@microsoft.com>
2022-10-26 14:50:16 +0100

    fixes

--- AzureServiceBus-Worker-Notification/Net.ServiceBus.Client/Program.cs
+++ AzureServiceBus-Worker-Notification/Net.ServiceBus.Client/Program.cs
@@ -1,6 +1,6 @@
-using Net.ServiceBus.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Net.ServiceBus.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
