66ddd5967506e2297d43da6567cf49d5f90f832e
Joao Goncalves <jp.reisgoncalves@gmail.com>
2022-11-23 12:45:22 +0000

    Features/service bus worker sample apps (#121)

---
+++ AzureServiceBus-Worker-Notification/Net.ServiceBus.Entities/Root.cs
@@ -0,0 +1,14 @@
+﻿namespace Net.ServiceBus.Entities
+{
+    public class Root
+    {
+        public string topic { get; set; }
+        public string subject { get; set; }
+        public string eventType { get; set; }
+        public string id { get; set; }
+        public Data data { get; set; }
+        public string dataVersion { get; set; }
+        public string metadataVersion { get; set; }
+        public DateTime eventTime { get; set; }
+    }
+}
---
+++ AzureServiceBus-Worker-Notification/Net.ServiceBus.Notification.Functions/SendNotificationActivity.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Net.ServiceBus.Notification.Functions
+{
+    public class SendNotificationActivity
+    {
+        private readonly ILogger<SendNotificationActivity> _log;
+
+        public SendNotificationActivity(ILogger<SendNotificationActivity> injectedLogger)
+        {
+            _log = injectedLogger;
+        }
+
+        [FunctionName(nameof(SendNotificationActivity))]
+        public async Task Run([ActivityTrigger] string name,
+            [SignalR(HubName = "notification")] IAsyncCollector<SignalRMessage> signalRMessages
+            )
+        {
+            await signalRMessages.AddAsync(
+                new SignalRMessage
+                {
+                    Target = "ReceiveMessage",
+                    Arguments = new[] { name }
+                });
+
+            _log.LogInformation("Mock Notification sent");
+        }
+    }
+}
