e6f87786904b6df5270425aa9a97fed7ab0241f1
goncalvesj <jp.reisgoncalves@gmail.com>
2024-03-05 12:44:20 +0000

    added app config and postgres.

--- AppService-FrontDoor-PrivateEndpoints/Program.cs
+++ AppService-FrontDoor-PrivateEndpoints/Program.cs
@@ -1,6 +1,9 @@
+using Azure.Data.AppConfiguration;
+using Azure.Identity;
 using Azure.Storage.Blobs;
 using Dapper;
 using Microsoft.Data.SqlClient;
+using Npgsql;
 using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,6 +23,44 @@ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
+app.MapGet("/testappconfig", async () =>
+{
+    var endpoint = builder.Configuration["APP_CONFIG_ENDPOINT"] ?? "";
+    var key = builder.Configuration["CONFIG_NAME"] ?? "";
+
+    var value = Guid.NewGuid().ToString();
+
+    var client = new ConfigurationClient(new Uri(endpoint), new DefaultAzureCredential());    
+    await client.SetConfigurationSettingAsync($"{key}-{value}", value);
+
+    return "Connection to Azure App Config successful. Config setting created.";
+})
+.WithName("TestAppConfigConnection")
+.WithOpenApi();
+
+app.MapGet("/testpostgres", async () =>
+{
+    try
+    {
+        var mySetting = configuration.GetValue<string>("POSTGRES_CONNECTIONSTRING");
+       
+        await using var conn = new NpgsqlConnection(mySetting);
+        await conn.OpenAsync();
+
+        await using var cmd = new NpgsqlCommand("SELECT version()", conn);
+        
+        var reader = await cmd.ExecuteScalarAsync();
+        
+        return $"Connection to Azure Postgres successful. Version: {reader}";
+    }
+    catch (Exception)
+    {
+        throw;
+    }
+})
+.WithName("TestPostresConnection")
+.WithOpenApi();
+
 app.MapGet("/testsql", async () =>
 {
     try
@@ -29,8 +70,8 @@ app.MapGet("/testsql", async () =>
         con.Open();
 
         var version = await con.ExecuteScalarAsync<string>("SELECT @@VERSION");
-
-        return version;
+                
+        return $"Connection to Azure SQL successful. Version: {version}";
     }
     catch (Exception)
     {
@@ -73,7 +114,7 @@ app.MapGet("/teststorage", async () =>
         BlobContainerClient container = new(mySetting, containerName);
         await container.CreateIfNotExistsAsync();
 
-        return "Connectivity test successful!";
+        return "Connection to Azure Storage successful. Blob container created.";
     }
     catch (Exception)
     {
