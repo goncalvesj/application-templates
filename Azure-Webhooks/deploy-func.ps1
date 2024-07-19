6134f34a0282557e0fe09a795536f445ad62910a
Joao Goncalves <jp.reisgoncalves@gmail.com>
2024-07-19 15:18:22 +0100

    webhooks updates.

--- Azure-Webhooks/deploy-func.ps1
+++ Azure-Webhooks/deploy-func.ps1
@@ -1,9 +1,10 @@
 # publish the code
-dotnet publish -c Release
-$publishFolder = "bin/Release/net8.0/publish"
+dotnet publish .\AzFunc.Webhook\AzFunc.Webhook.csproj -c Release
+
+$publishFolder = "AzFunc.Webhook/bin/Release/net8.0/publish"
 
 # create the zip
-$publishZip = "publish.zip"
+$publishZip = "function-deploy.zip"
 if(Test-path $publishZip) {Remove-item $publishZip}
 Add-Type -assembly "system.io.compression.filesystem"
 [io.compression.zipfile]::CreateFromDirectory($publishFolder, $publishZip)
@@ -11,5 +12,6 @@ Add-Type -assembly "system.io.compression.filesystem"
 # deploy the zipped package
 $functionAppName = "alz-shared-functions"
 $resourceGroup = "ALZ-LZ-Shared"
+
 az functionapp deployment source config-zip `
  -g $resourceGroup -n $functionAppName --src $publishZip
